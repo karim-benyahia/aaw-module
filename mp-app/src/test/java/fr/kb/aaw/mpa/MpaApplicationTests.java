ea80117f5602551010cd2b321a71b9e727efa3aa
karim <karim.benyahia@serli.com>
2022-04-28 23:50:05 +0200

    js available

--- mp-app/src/test/java/fr/kb/aaw/mpa/MpaApplicationTests.java
+++ mp-app/src/test/java/fr/kb/aaw/mpa/MpaApplicationTests.java
@@ -2,8 +2,10 @@ package fr.kb.aaw.mpa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = "dev")
 class MpaApplicationTests {
 
     @Test
