a9d83a01e97022cb69e96750bfd2cca8b8e3d85b
karim <karim.benyahia@serli.com>
2022-04-29 00:23:33 +0200

    js available

--- sp-app/server.js
+++ sp-app/server.js
@@ -8,7 +8,7 @@ const app = express(); // instantiate Express app
 
 app.use(express.json());
 
-app.use('/', express.static('dist'));
+app.use('/', express.static('sp-app/dist'));
 app.use('/', express.static('sp-app/public'));
 
 const apiRouter = require("./src/server/router/api-router");
