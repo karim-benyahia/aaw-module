e53e10e49e3aec481a8975f8fd5403253774311e
karim <karim.benyahia@serli.com>
2022-05-02 14:28:33 +0200

    index

--- sp-app/server.js
+++ sp-app/server.js
@@ -19,21 +19,21 @@ const PORT = process.env.PORT || 8082;
 app.get('/*', (req, res) => {
 
 
-    fs.readFile('./dist/index.html', 'utf8', function (err, html) {
+    fs.readFile('./src/app/index.html', 'utf8', function (err, html) {
         if (err) {
             console.error(err);
         } else {
-            // let result = (process.env.MODE !== "prod")
-            //     ? html
-            //         .replace('$js', 'http://localhost:8084/index.js')
-            //         .replace('$css', 'http://localhost:8084/index.css')
-            //
-            //     : html
-            //         .replace('$js', '/index.min.js')
-            //         .replace('$css', '/index.min.css')
+            let result = (process.env.MODE !== "prod")
+                ? html
+                    .replace('$js', 'http://localhost:8084/index.js')
+                    .replace('$css', 'http://localhost:8084/index.css')
+
+                : html
+                    .replace('$js', '/index.min.js')
+                    .replace('$css', '/index.min.css')
 
             res.writeHead(200, {"Content-Type": "text/html"});
-            res.end(html);
+            res.end(result);
         }
     });
 })
