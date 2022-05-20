3622a14b490b0615c76c73cab9c1d8576db5a964
karim <karim.benyahia@serli.com>
2022-05-20 12:23:59 +0200

    ajout de la pwa

--- sp-app/src/app/views/curse/curse.jsx
+++ sp-app/src/app/views/curse/curse.jsx
@@ -17,8 +17,8 @@ const curse = () => {
         <main>
             <iframe
                 src="https://docs.google.com/presentation/d/e/2PACX-1vRPe--Ct_YXZV9-MRZEhD377T1e5XHDjbqR08c6jaLeXp5c9QLFFDyEddtNdjqx0Q/embed?start=false&loop=false&delayms=60000"
-                frameBorder="0" width="100%" height="100%" allowFullScreen={true} mozallowfullscreen={true}
-                webkitallowfullscreen={true}></iframe>
+                frameBorder="0" width="100%" height="100%" allowFullScreen={true} mozallowfullscreen="true"
+                webkitallowfullscreen="true"></iframe>
         </main>
             <Footer author="Karim Benyahia" curse="AAW"/>
         </div>
